0fbf701183fc4fd18c59e7b83c89c695f9bf3159
Tan Wei Yang <weiyang.tan@pixelstack.my>
2020-12-16 22:28:00 +0800

    :wastebasket: clean up test codes

--- src/Application.ts
+++ src/Application.ts
@@ -8,18 +8,6 @@ export class Application {
         this.m_Engine = new Engine(canvas);
         this.m_Scene = new Scene(this.m_Engine);
 
-        let camera = new FreeCamera("camera1", new Vector3(0,5,-10), this.m_Scene);
-        camera.setTarget(Vector3.Zero());
-        camera.attachControl(canvas, true);
-
-        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-        var light = new HemisphericLight("light1", new Vector3(0, 1, 0), this.m_Scene);
-        // Default intensity is 1. Let's dim the light a small amount
-        light.intensity = 0.7;
-        
-        // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-        var sphere = Mesh.CreateSphere("sphere1", 16, 2, this.m_Scene);
-
         // Render every frame
         this.m_Engine.runRenderLoop(() => {
             this.m_Scene.render();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Application } from "./Application";
 
-const canvas = document.getElementById("renderCanvas");
-
-let app = new Application(<HTMLCanvasElement>canvas);
+window.addEventListener("DOMContentLoaded", () => {
+    const canvas = document.getElementById("renderCanvas");
+    let app = new Application(<HTMLCanvasElement>canvas);
+});
